0f8fa05d4d94d3d1189e595d5ceb3b7f3497af9b
Andreas Müller <mail@devmount.de>
2022-01-20 21:14:57 +0100

    💚 unify build css file

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,11 @@ module.exports = {
 			chunkFilename: "js/[name].bundle.js",
 		},
 	},
+	css: {
+		extract: {
+			filename: 'css/[name].app.css'
+		}
+	},
 	productionSourceMap: false,
 	publicPath: '/'
 }
